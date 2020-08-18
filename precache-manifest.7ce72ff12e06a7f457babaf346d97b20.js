5a529b41ce42132a6cd5ee624c46bd2e98880651
Denys Shutyi <Mufasishe@gmail.com>
2020-08-17 23:52:09 -0700

    Updates

---
+++ precache-manifest.7ce72ff12e06a7f457babaf346d97b20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76decca1fde744f49a0d8c82f71ed7c0",
+    "url": "/todowithserver/index.html"
+  },
+  {
+    "revision": "65b63afdaba7ea20d030",
+    "url": "/todowithserver/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "9c5c6fb62d02e7f1e3f4",
+    "url": "/todowithserver/static/css/main.c17d5aff.chunk.css"
+  },
+  {
+    "revision": "65b63afdaba7ea20d030",
+    "url": "/todowithserver/static/js/2.07696a72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todowithserver/static/js/2.07696a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c5c6fb62d02e7f1e3f4",
+    "url": "/todowithserver/static/js/main.5b870ef9.chunk.js"
+  },
+  {
+    "revision": "6c8998f8193514bccc7e",
+    "url": "/todowithserver/static/js/runtime-main.f6752414.js"
+  },
+  {
+    "revision": "6b3bccc7d3d372367a941f78131f785d",
+    "url": "/todowithserver/static/media/paper.6b3bccc7.jpg"
+  }
+]);
